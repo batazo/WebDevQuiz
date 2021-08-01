ff88fb0c0f5b56c317038e6698b5aad0ada2f957
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-08-01 23:10:56 +0200

    New functions and refactoring

--- Frontend-1/js/main.js
+++ Frontend-1/js/main.js
@@ -1,72 +1,39 @@
-const version = "v9"
+//Globals
+const version = "v10";
 let answerareas;
 let qinputs;
 
+//Inits
+renerQuestion();
 verSpan.innerHTML = `(${version})`;
 
-async function getQuestion() {
-  const endpoint = "https://webdevquiz.mysqhost.ml:8090/api/questionlist/10";
-
-  const options = {
-    method: "GET",
-    mode: "cors"
-  };
-
-  const response = await fetch(endpoint, options);
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
-
-async function checkAnswer(answerareas) {
-  const endpoint = "https://webdevquiz.mysqhost.ml:8090/api/check/";
-  let checkedAnswers = document.querySelectorAll(`#${answerareas.id} input:checked`)
-  let selected = []
-    for(let checkedAnswer of checkedAnswers){
-      selected.push(checkedAnswer.dataset.aid)
-    }
-    
-  const bodyData = {
-      questionId: answerareas.dataset.qid,
-      selectedAnswerIds: selected
-   }
+//Renders
+async function renderEvaluation(answerareas) {
+  answerareas.nextElementSibling.innerHTML = "";
+  let qid = getAnswerareaDatasetQid(answerareas);
+  let aids = getAnswerareaDatasetAids(answerareas);
   
-  const options = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(bodyData)
-  };
-  const response = await fetch(endpoint, options);
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
-
-async function renderEvaluation(answerareas){
-  answerareas.nextElementSibling.innerHTML = ""
-  let data = await checkAnswer(answerareas)
-  let explan = (data.explanation == undefined)? 'Nem jelöltél ki választ' : data.explanation
-  let correct = (data.correct)? 'A válaszod helyes!' : 'A válaszod sajnos helytelen!'
-  let correctColor = (data.correct)? 'green' : 'red'
-  let whatCorrects = (data.correctAnswers == undefined)? '' : data.correctAnswers
-  let whatCorrectString = ""
-  if(whatCorrects)
-      whatCorrectString = "A helyes válasz(ok):"
-      for(let whatCorrect of whatCorrects){ whatCorrectString += ' ' +whatCorrect.answer }
+  let data = await checkAnswer(qid, aids);
+  let explan = data.explanation == undefined ? "Nem jelöltél ki választ" : data.explanation;
+  let correct = data.correct ? "A válaszod helyes!" : "A válaszod sajnos helytelen!";
+  let correctColor = data.correct ? "green" : "red";
+  let whatCorrects = data.correctAnswers == undefined ? "" : data.correctAnswers;
+  let whatCorrectString = "";
+  if (whatCorrects) whatCorrectString = "A helyes válasz(ok):";
+  for (let whatCorrect of whatCorrects) {
+    whatCorrectString += " " + whatCorrect.answer;
+  }
   let renderString = `
       <p><b style="color: ${correctColor}">${correct}</b> ${whatCorrectString}</p>
       <p>${explan}</p>
-  `
+  `;
   answerareas.nextElementSibling.innerHTML += renderString;
-  answerareas.nextElementSibling.classList.remove('hidden')
+  answerareas.nextElementSibling.classList.remove("hidden");
 }
 
-function renderAllEvaluation(){
-  for(let answerarea of answerareas){
-    renderEvaluation(answerarea)
+function renderAllEvaluation() {
+  for (let answerarea of answerareas) {
+    renderEvaluation(answerarea);
   }
 }
 
@@ -87,7 +54,7 @@ async function renerQuestion() {
 				<h3 id="Q${questionNumber}"> ${questionNumber}. Kérdés: ${question.question}</h3>
         <div class="answerarea" id="answerarea-${questionNumber}" data-qid="${question.id}">`;
       for (answer of question.answers) {
-        answerNumber++
+        answerNumber++;
         renderString += `
           <input type="radio" name="Q${questionNumber}" id="Q${questionNumber}A${answerNumber}" data-aid="${answer.id}" />
 					<label for="Q${questionNumber}A${answerNumber}">${answer.answer}</label>`;
@@ -96,6 +63,7 @@ async function renerQuestion() {
       </div><!-- end of answerarea -->
         <div class="explanation hidden">
         </div>
+        <hr>
 			</div><!-- questioninput end -->`;
     }
   } catch (e) {
@@ -103,12 +71,74 @@ async function renerQuestion() {
   }
 
   questionDIV.innerHTML = renderString;
-  refreshSelectors()
+  refreshSelectors();
 }
 
-function refreshSelectors(){
-  answerareas = document.querySelectorAll('[data-qid]')
-  qinputs = document.querySelectorAll('.questioninput')
+//Helpers
+function refreshSelectors() {
+  answerareas = document.querySelectorAll("[data-qid]");
+  qinputs = document.querySelectorAll(".questioninput");
 }
 
-renerQuestion();
+function getAnswerareaDatasetQid(answerareas) {
+  return answerareas.dataset.qid;
+}
+
+function getAnswerareaDatasetAids(answerareas) {
+  let checkedAnswers = document.querySelectorAll(
+    `#${answerareas.id} input:checked`
+  );
+  let selected = [];
+  for (let checkedAnswer of checkedAnswers) {
+    selected.push(checkedAnswer.dataset.aid);
+  }
+  return selected;
+}
+
+function getGoodAnswersFromData(fromdata){
+  let goodAnswers = [];
+  for(data of fromdata.correctAnswers){
+    goodAnswers.push(data.id);
+  }
+  return goodAnswers;
+}
+
+function getGoodAnswersStringFromData(fromdata){
+  let goodAnswers = "";
+  for(data of fromdata.correctAnswers){
+    goodAnswers += data.answer;
+  }
+  return goodAnswers;
+}
+
+//Data Queries
+async function getQuestion() {
+  const endpoint = "https://webdevquiz.mysqhost.ml:8090/api/questionlist/10";
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+async function checkAnswer(qid, aids) {
+  const endpoint = "https://webdevquiz.mysqhost.ml:8090/api/check/";
+  
+  const bodyData = {
+    questionId: qid,
+    selectedAnswerIds: aids
+  };
+
+  const options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(bodyData)
+  };
+  const response = await fetch(endpoint, options);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
